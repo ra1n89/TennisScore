86cd700a7ca96cdb17e8a284af25305e8030cdfa
ra1n89nout <prorain88@gmail.com>
2024-12-01 17:31:43 +0500

    add: first servlet

---
+++ src/main/java/ru/prorain/servlets/NewMatchServlet.java
@@ -0,0 +1,17 @@
+package ru.prorain.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/new-match")
+public class NewMatchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("sdasdsa");
+    }
+}
