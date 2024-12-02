b8b565d154e6dcd31811025f957f2727f2d94c11
Ahmetzyanov <prorain88@gmail.com>
2024-12-03 00:51:28 +0500

    refactor: move sessionFactory creating to ConnectionManager

--- src/main/java/ru/prorain/repository/UserRepository.java
+++ src/main/java/ru/prorain/repository/UserRepository.java
@@ -5,44 +5,29 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
 import ru.prorain.entity.User;
+import ru.prorain.utils.ConnectionManager;
 
 import java.util.Collection;
 
 
 public class UserRepository implements CrudRepository<User, Integer>{
-    SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
+
+    SessionFactory sessionFactory = ConnectionManager.getSessionFactory();
 
     private static final UserRepository USER_REPOSITORY = new UserRepository();
 
-
     private UserRepository(){
-        String sql = """
-        CREATE TABLE Players (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR UNIQUE
-        )
-        """;
-        Session currentSession = sessionFactory.getCurrentSession();
-        currentSession.beginTransaction();
-        NativeQuery<User> query = currentSession.createNativeQuery(sql).addEntity(User.class);
-        query.executeUpdate();
-        currentSession.getTransaction().commit();
-
     }
-
 
     @Override
     public User save(User id) {
 
-
-
-
         Session currentSession = sessionFactory.getCurrentSession();
-        currentSession.beginTransaction();
-        //NativeQuery<User> query = currentSession.createNativeQuery(sql).addEntity(User.class);
 
+        currentSession.beginTransaction();
         currentSession.persist(new User("Anton"));
         currentSession.getTransaction().commit();
+
         getOne();
         return null;
     }
@@ -60,9 +45,11 @@ public class UserRepository implements CrudRepository<User, Integer>{
     @Override
     public User getOne() {
         Session currentSession = sessionFactory.openSession();
+
         currentSession.beginTransaction();
         User user = currentSession.get(User.class, 1);
         currentSession.getTransaction().commit();
+
         System.out.println(user);
         return null;
     }
--- src/main/java/ru/prorain/utils/ConnectionManager.java
+++ src/main/java/ru/prorain/utils/ConnectionManager.java
@@ -1,8 +1,12 @@
 package ru.prorain.utils;
+import lombok.Getter;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.prorain.entity.User;
 
 public class ConnectionManager {
-SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
+
+    @Getter
+    static SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
+
 }
