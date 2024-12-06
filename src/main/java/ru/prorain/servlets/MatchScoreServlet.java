6c4ee9e68b02bbcee61c52f028220a996f28a948
ra1n89nout <prorain88@gmail.com>
2024-12-06 23:03:41 +0500

    add: MatchScoreServlet

---
+++ src/main/java/ru/prorain/servlets/MatchScoreServlet.java
@@ -0,0 +1,9 @@
+package ru.prorain.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/match-score")
+public class MatchScoreServlet extends HttpServlet {
+
+}
