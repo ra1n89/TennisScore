c66f8c5bc9c05ef1e70c635ff2a7f56cc6fe2a6b
Ahmetzyanov <prorain88@gmail.com>
2024-12-07 23:55:51 +0500

    add: doPost and doGet methods

--- src/main/java/ru/prorain/servlets/MatchScoreServlet.java
+++ src/main/java/ru/prorain/servlets/MatchScoreServlet.java
@@ -1,9 +1,32 @@
 package ru.prorain.servlets;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.prorain.dto.MatchDto;
+import ru.prorain.service.UserService;
 
-@WebServlet("/match-score")
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/match-score/*")
 public class MatchScoreServlet extends HttpServlet {
+    UserService userService = UserService.getInstance();
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        MatchDto match = userService.getMatch(uuid);
+        req.setAttribute("match", match);
+        req.getRequestDispatcher("match-score.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        System.out.println(id);
+    }
 }
