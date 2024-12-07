19f214a70c11d4e415fc65bfb49f35675169f71c
Ahmetzyanov <prorain88@gmail.com>
2024-12-07 23:55:23 +0500

    refactor: change user saving method

--- src/main/java/ru/prorain/service/UserService.java
+++ src/main/java/ru/prorain/service/UserService.java
@@ -1,33 +1,37 @@
 package ru.prorain.service;
 
+import ru.prorain.dto.MatchDto;
 import ru.prorain.entity.User;
 import ru.prorain.repository.CrudRepository;
 import ru.prorain.repository.UserRepository;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class UserService {
 
     private static final UserService USER_SERVICE = new UserService();
     CrudRepository<User, Integer> crudRepository = UserRepository.getInstance();
-
+    ConcurrentMap<UUID, MatchDto> concurrentHashMap = new ConcurrentHashMap<>();
 
     private UserService() {
 
     }
 
-    public User save(User firstPlayer, User secondPlayer) {
-        try {
-            crudRepository.save(firstPlayer);
-        } catch (Exception e) {
-            e.getMessage();
-        }
-        try {
-            crudRepository.save(secondPlayer);
-        } catch (Exception e) {
-            e.getMessage();
-        }
-        return null;
+    public MatchDto save(User firstPlayer, User secondPlayer) {
+        User firstPlayerWithId;
+        User secondPlayerWithId;
+
+            firstPlayerWithId = crudRepository.save(firstPlayer);
+            secondPlayerWithId = crudRepository.save(secondPlayer);
+
+        MatchDto match = new MatchDto(firstPlayerWithId.getId(), secondPlayerWithId.getId(),0,0 );
+        concurrentHashMap.put(match.getId(), match);
+        return match;
     }
 
 
@@ -43,6 +47,14 @@ public class UserService {
     public User getOne() {
 
         return null;
+    }
+
+    public MatchDto getMatch(UUID uuid){
+
+        return concurrentHashMap.get(uuid);
+    }
+    public ConcurrentMap<UUID, MatchDto> getList(){
+        return concurrentHashMap;
     }
 
     public static UserService getInstance() {
