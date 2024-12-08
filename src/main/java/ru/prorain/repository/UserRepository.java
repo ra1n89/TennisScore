d37ab83b48fe5d9d67fa44a7451676eac452d8f9
Ahmetzyanov <prorain88@gmail.com>
2024-12-08 19:04:59 +0500

    feat: add loggin to methods

--- src/main/java/ru/prorain/repository/UserRepository.java
+++ src/main/java/ru/prorain/repository/UserRepository.java
@@ -3,8 +3,7 @@ package ru.prorain.repository;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.NativeQuery;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.prorain.entity.User;
 import ru.prorain.exception.DatabaseException;
@@ -12,58 +11,69 @@ import ru.prorain.exception.EntiteExistingException;
 import ru.prorain.utils.ConnectionManager;
 
 import java.util.Collection;
-import org.slf4j.Logger;
 
 
-public class UserRepository implements CrudRepository<User, Integer>  {
+public class UserRepository implements CrudRepository<User, Integer> {
 
-    Logger logger = LoggerFactory.getLogger(UserRepository.class);
+    Logger log = LoggerFactory.getLogger(UserRepository.class);
     SessionFactory sessionFactory = ConnectionManager.getSessionFactory();
 
     private static final UserRepository USER_REPOSITORY = new UserRepository();
 
-    private UserRepository(){
+    private UserRepository() {
     }
 
     @Override
     public User save(User user) {
+        log.info("Saving user in class with method save()");
 
         try (Session currentSession = sessionFactory.getCurrentSession()) {
 
-
+            log.debug("Start saving user: {}, start open transaction", user);
             currentSession.beginTransaction();
-
+            log.debug("Transaction opened");
+            log.debug("Check if the User exist in DB");
             User existingUser = currentSession.createQuery("FROM User WHERE name = :name", User.class)
                     .setParameter("name", user.getName())
                     .uniqueResult();
+
             if (existingUser != null) {
+                log.debug("User exists already");
                 return existingUser;
             }
-
+            log.debug("User doesnt exist, saving to DB");
             currentSession.persist(user);
             currentSession.getTransaction().commit();
+            log.debug("User saved to DB successfully");
         } catch (HibernateException e) {
-            if(e.getMessage().contains("Unique index or primary key violation")){
+            log.debug("Exception");
+            if (e.getMessage().contains("Unique index or primary key violation")) {
+                log.debug("Unique index or primary key violation");
                 throw new EntiteExistingException("PLayer with name: " + user.getName() + " already exists");
-            };
+            }
+            ;
+            log.debug("Another exception occurred");
             throw new DatabaseException("Failed to save player with name: " + user + "in database");
         }
-        getOne();
+
         return user;
     }
 
     @Override
     public User update(User t) {
+        log.info("Updating user in class with method update()");
         return null;
     }
 
     @Override
     public Collection getAll() {
+        log.info("Getting all users in class with method getAll()");
         return null;
     }
 
     @Override
     public User getOne() {
+        log.info("Getting user by Id in class with method getOne()");
         Session currentSession = sessionFactory.openSession();
 
         currentSession.beginTransaction();
@@ -74,7 +84,8 @@ public class UserRepository implements CrudRepository<User, Integer>  {
         return null;
     }
 
-    public static UserRepository getInstance(){
+    public static UserRepository getInstance() {
+
         return USER_REPOSITORY;
     }
 }
