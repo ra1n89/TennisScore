834290210c0e458afeeeee23783b1693e6c8a644
ra1n89nout <prorain88@gmail.com>
2024-12-22 13:44:49 +0500

    refactor: use method and try catch in DB initializing

--- src/main/java/ru/prorain/utils/ConnectionManager.java
+++ src/main/java/ru/prorain/utils/ConnectionManager.java
@@ -1,19 +1,31 @@
 package ru.prorain.utils;
-import lombok.Getter;
+
 import lombok.experimental.UtilityClass;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.prorain.entity.Match;
 import ru.prorain.entity.User;
+import ru.prorain.exception.DatabaseException;
 
 @UtilityClass
 public class ConnectionManager {
 
-    @Getter
-    static SessionFactory sessionFactory = new Configuration()
-            .configure("hibernate.cfg.xml")
-            .addAnnotatedClass(User.class)
-            .addAnnotatedClass(Match.class)
-            .buildSessionFactory();
+    private SessionFactory sessionFactory;
 
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(User.class)
+                        .addAnnotatedClass(Match.class)
+                        .buildSessionFactory();
+                return sessionFactory;
+            } catch (HibernateException e){
+                throw new DatabaseException("Error in Session Factory initialization");
+            }
+        }
+        return sessionFactory;
+    }
 }
