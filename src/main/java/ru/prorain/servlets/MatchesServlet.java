a54002e39f327e62bc1d401d0d83d59dfd4246a7
ra1n89nout <prorain88@gmail.com>
2024-12-22 13:44:06 +0500

    refactor: change filter using DB instead filtering after getAll

--- src/main/java/ru/prorain/servlets/MatchesServlet.java
+++ src/main/java/ru/prorain/servlets/MatchesServlet.java
@@ -6,29 +6,29 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ru.prorain.entity.Match;
-import ru.prorain.entity.User;
 import ru.prorain.service.FinishMatchPersistenceService;
 import ru.prorain.service.OngoingMatchService;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @WebServlet("/matches/*")
 public class MatchesServlet extends HttpServlet {
 
     FinishMatchPersistenceService matchService = FinishMatchPersistenceService.getInstance();
     OngoingMatchService ongoingMatchService = OngoingMatchService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<Match> matchList = matchService.getAll();
+        List<Match> matchList;
         String filterByPlayerName = req.getParameter("filter_by_player_name");
-        if (!Objects.equals(filterByPlayerName, "") && filterByPlayerName != null ) {
-            matchList = matchList.stream()
-                    .filter(m -> (m.getPlayer1().getName().equals(filterByPlayerName) || m.getPlayer2().getName().equals(filterByPlayerName)))
-                    .collect(Collectors.toList());
+
+        if (!Objects.equals(filterByPlayerName, "") && filterByPlayerName != null) {
+            matchList = matchService.getFilteredMatchesByPlayerName(filterByPlayerName);
+        } else {
+            matchList = matchService.getAll();
         }
         req.setAttribute("list", matchList);
         req.getRequestDispatcher("/matches.jsp").forward(req, resp);
